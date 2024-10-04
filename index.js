31a35a1b699d608704b40aa093024226084e79dc
Beth Kimani <kbwaceke@gmail.com>
2024-10-04 20:06:17 +0300

    Add javascript array filter

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+// 1. findMatching function
+function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+  }
+  
+  // 2. fuzzyMatch function
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+  }
+  
+  // 3. matchName function
+  function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name === query);
+  }
+  
